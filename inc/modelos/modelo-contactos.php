bd6ecad742f66d375c54e25703c9f4a90dc844ff
lekanda <abernaolao@gmail.com>
2021-10-10 18:54:43 +0200

    666. Traer los datos del POST a JS.

---
+++ inc/modelos/modelo-contactos.php
@@ -0,0 +1,4 @@
+<?php
+
+// Crea un arreglo asociativo con el POST del form.
+echo json_encode($_POST);
--- js/app.js
+++ js/app.js
@@ -11,29 +11,69 @@ function eventListeners() {
 
 function leerFormulario(e) { 
     e.preventDefault();// previene action del form
-    // console.log(e);
-    // console.log('Presionaste');
-
     // Leer datos de los inputs
     const nombre = document.querySelector('#nombre').value,
           empresa = document.querySelector('#empresa').value,
-          telefono = document.querySelector('#telefono').value;
+          telefono = document.querySelector('#telefono').value,
+          accion = document.querySelector('#accion').value;
     // console.log(nombre);
     // console.log(empresa);
     // console.log(telefono);
 
-
     if (nombre === '' || empresa === '' || telefono === '') {
-        // console.log('Los campos estan vacios');
-
         // 2 parametros: txt y clase
         mostrarNotificacion('Todos los campos son obligatorios', 'error');
     } else {
-        // console.log('Tienen algo');
+        //Pasa la validacion, llamar a DB con AJAX.
+        const  infoContacto = new FormData();// Para leer datos de 1 formulario.
+        infoContacto.append('nombre', nombre);
+        infoContacto.append('empresa', empresa);
+        infoContacto.append('telefono', telefono);
+        infoContacto.append('accion', accion);
 
-        mostrarNotificacion('Datos correctos', 'correcto');
+        // Para poder leer los datos del formulario hacerlo con los 3 puntos(...). Si no, no deja leerlos.
+        // console.log(...infoContacto);
+
+        if (accion === 'crear') {
+            // Crear un nuevo contacto
+            insertarDB(infoContacto);
+        } else {
+            // Editar el contacto
+
+        }
+
+        // mostrarNotificacion('Datos correctos', 'correcto');
     }
 }
+
+/* Inserta en la DB con AJAX */
+function insertarDB(datos) {
+    // Llamar a AJAX
+    // Crear el Objeto
+    const xhr = new XMLHttpRequest();
+    // Abrir la conexion
+    xhr.open('POST','inc/modelos/modelo-contactos.php', true);
+    // Pasar los datos 
+    xhr.onload = function () {
+        if (this.status === 200) {
+            // console.log(JSON.parse(xhr.responseText));// Convierte el string que viene de modelo-contactos.php a JSON.
+
+            // Leemos la respuesta de PHP
+            const respuesta = JSON.parse(xhr.responseText);
+            // console.log(respuesta['empresa']);
+            // console.log(respuesta.empresa);
+
+
+        }
+    }
+    // Enviar los datos
+    xhr.send(datos);
+}
+
+
+
+
+
 
 
 // Notificacion en pantalla de falta de datos en Form.
