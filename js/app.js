4300cdf7633b6ba5044ac51107d9b7be9664325c
lekanda <abernaolao@gmail.com>
2021-10-10 14:47:21 +0200

    665.Crear notificacion en JS cuando form esta vacio.

--- js/app.js
+++ js/app.js
@@ -18,25 +18,40 @@ function leerFormulario(e) {
     const nombre = document.querySelector('#nombre').value,
           empresa = document.querySelector('#empresa').value,
           telefono = document.querySelector('#telefono').value;
-
-    if (nombre === '' || empresa === '' || telefono === '') {
-        console.log('Los campos estan vacios');
-    } else {
-        console.log('Tienen algo');
-    }
-
-
-
-
-
-    // const empresa = document.querySelector('#empresa').value;
-    // const telefono = document.querySelector('#telefono').value;
     // console.log(nombre);
     // console.log(empresa);
     // console.log(telefono);
 
 
+    if (nombre === '' || empresa === '' || telefono === '') {
+        // console.log('Los campos estan vacios');
+
+        // 2 parametros: txt y clase
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
+    } else {
+        // console.log('Tienen algo');
+
+        mostrarNotificacion('Datos correctos', 'correcto');
+    }
+}
 
 
+// Notificacion en pantalla de falta de datos en Form.
+function mostrarNotificacion(mensaje,clase){
+    const notificacion = document.createElement('div');
+    notificacion.classList.add(clase,'notificacion','sombra');
+    notificacion.textContent = mensaje;
 
+    //Formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+    // Ocultar y mostrar la notificacion.
+    setTimeout(() => {
+        notificacion.classList.add('visible');
+        setTimeout(() => {
+            notificacion.classList.remove('visible');
+            setTimeout(() => {
+                notificacion.remove(); 
+            }, 500);
+        }, 3000);
+    }, 100);
 }
